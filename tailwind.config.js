5e548f13cc54b5d0b7a768daf3db5ada45561106
Siravit Dontumpai <siravitdeil19@gmail.com>
2022-01-19 20:49:39 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  purge: ["./public/**/*.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        "h-blue": "#676FA3",
+        "b-blue": "#CDDEFF",
+        "b2-blue": "#EEF2FF",
+        "t-red": "#FF5959",
+      },
+    },
+    container: {
+      center: true,
+      padding: "2rem",
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
